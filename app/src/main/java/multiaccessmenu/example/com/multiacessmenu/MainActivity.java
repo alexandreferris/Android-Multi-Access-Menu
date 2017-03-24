868cda3ef6562eb84ce760b998f5f1086bf1dad2
Alexandre Ferris <ferris.alexandre@gmail.com>
2017-03-24 14:04:11 -0300

    start of project

--- app/src/main/java/multiaccessmenu/example/com/multiacessmenu/MainActivity.java
+++ app/src/main/java/multiaccessmenu/example/com/multiacessmenu/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.i("a","b");
+        Log.i("b","c");
     }
 }
