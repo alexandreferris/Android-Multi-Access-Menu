4ad9e9ca5bd60fd450424b36b65fecd18ebbb336
Alexandre Ferris <ferris.alexandre@gmail.com>
2017-03-24 14:08:33 -0300

    Default configurations

---
+++ app/src/main/java/multiaccessmenu/example/com/multiacessmenu/BaseActivity.java
@@ -0,0 +1,11 @@
+package multiaccessmenu.example.com.multiacessmenu;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by alexandre ferris on 24/03/17.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+
+}
